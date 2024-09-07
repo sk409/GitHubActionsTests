60e69010830914d025989ebccfc7daa5b882d34c
sk409 <deepstrap353@gmail.com>
2024-09-07 15:42:35 +0900

    xcode

---
+++ Packages/GitHubActions/Tests/GitHubActionsTests/GitHubActionsTestsTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+
+@testable import GitHubActions
+
+final class GitHubActionsTestsTests: XCTestCase {
+    func testExample() throws {
+        if #available(iOS 17, *) {
+            XCTAssertEqual(f(a: 1), 2)
+        } else {
+            XCTAssertTrue(false)
+        }
+    }
+}
