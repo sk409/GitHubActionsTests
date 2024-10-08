5dd0f967a12527c44456e8be9395595a42663199
sk409 <deepstrap353@gmail.com>
2024-10-08 13:58:53 +0900

    t

---
+++ Packages/GitHubActions/Tests/GitHubActionsTests/GitHubActionsTestsTests.swift
@@ -0,0 +1,11 @@
+import ComposableArchitecture
+import XCTest
+
+@testable import GitHubActions
+
+@MainActor
+final class GitHubActionsTestsTests: XCTestCase {
+  func testExample() throws {
+    XCTAssertTrue(false)
+  }
+}
