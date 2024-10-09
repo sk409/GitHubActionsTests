31ca19693e3068e03db373d6da53e0a79d4aac21
sk409 <deepstrap353@gmail.com>
2024-10-09 10:12:47 +0900

    t

--- p.sh
+++ p.sh
@@ -4,9 +4,6 @@ git checkout main
 git add .
 git commit -m "t"
 git push origin main
-git checkout master
-git rebase master
-git push --force-with-lease origin master
-git checkout combined/4.6.3-release-branch
-git rebase master
-git push --force-with-lease origin combined/4.6.3-release-branch
+git checkout feature/pr40
+git rebase main
+git push --force-with-lease origin feature/pr40
